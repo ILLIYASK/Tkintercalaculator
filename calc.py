e5ba8aeb165962ebd7f91919f7cadd45325fe821
ILLIYASK <Illiyask26061999@gmail.com>
2022-11-23 13:09:31 +0530

    label is created

--- calc.py
+++ calc.py
@@ -2,7 +2,9 @@ import tkinter
 
 
 window=tkinter.Tk()
-window.geometry('800x600')
+window.geometry('800x650')
 window.title('Calculator'.center(230))
-window.config(bg='#42403a')
+window.config(bg='black')
+lb=tkinter.Label(window,height=9,width=105,text='this is label',bg='#d8dee8',anchor="se")
+lb.place(x=30,y=30,)
 window.mainloop()
